660f1fabbe6507b0f2b35b26aff7db47bc4a6dd6
JM <wkdwoals96@gmail.com>
2021-06-10 21:42:08 +0900

    vue tokken ex 0610 -jm

---
+++ playground/src/components/AppNav.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="nav">
+        <router-link to="/">
+            Home
+        </router-link>
+        <router-link to="/dashboard">
+            Dashboard
+        </router-link>
+    </div>
+</template>
